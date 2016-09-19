e588788f7d59e2f6b015d0600f6d06fa98f66ec4
Walking Disaster <michaelmeadows@gmail.com>
2016-09-19 12:38:09 -0400

    foo

---
+++ src/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using Newtonsoft.Json;
+using WebApplication1.ViewModels.Home;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public string Foo(string data)
+        {
+            var foo = JsonConvert.DeserializeObject<FooViewModel>(data);
+            return data.ToString();
+        }
+    }
+}
---
+++ src/WebApplication1/ViewModels/Home/FooViewModel.cs
@@ -0,0 +1,32 @@
+﻿namespace WebApplication1.ViewModels.Home
+{
+    public class FooViewModel
+    {
+        public string SendThankYouEmail { get; set; }
+        public BasicInfoViewModel BasicInfo { get; set; }
+        public string PreferredDay { get; set; }
+
+        public string PreferredTime { get; set; }
+
+        public bool FormIsLicensedAgent { get; set; }
+
+        public bool FormEmailOptIn { get; set; }
+        /*
+            "SendThankYouEmail": "True",
+            "BasicInfo.FullName": "Shawn Lee",
+            "BasicInfo.EmailAddress": "(662) 622-6263",
+            "BasicInfo.QuestionOrComments": "asdf",
+            "PreferredDay": "Wednesday",
+            "PreferredTime": "Afternoon",
+            "FormIsLicensedAgent": "true",
+            "FormEmailOptIn": "true"
+        */
+    }
+
+    public class BasicInfoViewModel
+    {
+        public string FullName { get; set; }
+        public string EmailAddress { get; set; }
+        public string QuestionOrComments { get; set; }
+    }
+}
